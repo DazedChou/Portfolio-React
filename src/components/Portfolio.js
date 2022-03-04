63e1a776496c839b1065323e637c4de6f0c50508
David Chou <davidchou2009@gmail.com>
2022-03-04 09:35:22 -0800

    import all components, added ability to conditionally render page using states.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,25 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import About from './pages/About';
+import Header from './Header';
+import Contact from './pages/Contact';
+import Project from './pages/Project';
+import Resume from './pages/Resume';
+import Nav from './Nav';
+import Footer from './Footer';
+// import { findByLabelText } from '@testing-library/react';
+// import Background from './images/background.jpg'
+
+const styles = {
+    header: {
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "gray"
+    }
+}
 
 function Portfolio() {
-    const [result, setResult] = useState([]);
+    // const [result, setResult] = useState([]);
     // const myRepos = async () => {
-    useEffect(() => {
-        axios.get(`https://api.github.com/users/dazedchou/repos`)
-            .then((res) => {
-                console.log(res.data);
-                setResult(res.data);
-            });
-    }, []);
+    // useEffect(() => {
+    //     axios.get(`https://gh-pinned-repos.egoist.sh/?username=dazedchou`)
+    //         .then((res) => {
+    //             console.log(res.data);
+    //             setResult(res.data);
+    //         });
+    // }, []);
 
-    // };
-    // myRepos();
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Project') {
+            return <Project/>;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
 
     return (
-        <div>
-            {result.map((repo) => (
-                <div key={repo.id}>{repo.name}</div>
-            ))}
+        <div style={{background:'#050f21', color:'#8bace8', height: '120vh'}} id='portfolio'>
+            {/* <Header currentPage={currentPage} handlePageChange={handlePageChange}/> */}
+            <div style={styles.header} >
+            <Header />
+            <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+            </div>
+            {renderPage() }
+            {/* {result.map((repo) => (
+                <div key={Math.floor(Math.random() * 10000)}>{repo.repo}</div>
+            ))} */}
+            {!currentPage==='Resume' ? <Footer /> : <></>}
         </div>
     )
 };
