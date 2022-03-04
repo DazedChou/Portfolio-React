2b243e22b467e826272abde1894c0367be3ff1f2
David Chou <davidchou2009@gmail.com>
2022-03-04 09:38:17 -0800

    Create Resume component that loads the pdf viewer

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Resume() {
+
+    return (
+
+        <iframe title="resume" src="./ChouResume.pdf" style={{width: '100%', height: '100%', border: 'none', paddingBottom:'2.5rem'}}></iframe>
+
+        // <Document file={file} >
+        //     <Page  />
+        // </Document>
+
+
+    );
+};
+
+export default Resume;
