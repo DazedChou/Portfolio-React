6292b9b08b87b41bf5e24f8c0d11438faa01f2f6
David Chou <davidchou2009@gmail.com>
2022-03-04 18:00:10 -0800

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+            <footer className="col d-flex flex-row justify-content-center" id='footer'>
+                <a className="mx-3" href="https://github.com/DazedChou" target="_blank" rel="noreferrer"><i
+                    className="fab fa-github fa-3x"></i></a>
+                <a className="mx-3" href="https://www.linkedin.com/in/Davidchou99/" target="_blank" rel="noreferrer"><i
+                    className="fab fa-linkedin fa-3x"></i></a>
+            </footer>
+    )
+};
+
+export default Footer;
